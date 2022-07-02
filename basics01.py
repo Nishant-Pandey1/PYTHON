b39a5fef5750a4388d66840d0006beddc06de46e
Nishant <pandeynishnu01@gmail.com>
2022-07-02 22:29:01 +0545

    It;s a Basic of PYTHON

---
+++ basics01.py
@@ -0,0 +1,32 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+print("X is an", type(x))
+print("Y is a",type(y))
+
+
+#Adding Up Numbers
+
+x = int(input("Enter the number:"))
+y = int(input("Enter the second number"))
+z = x + y
+print("The sum of given number is :", z)
+
+
+# "" and '' is same in PYTHON
+# ""/'' is used for STRINGS
+
+x ="Fans"
+y ='Fans'
+if x == y:
+    a = int(1)
+    print(a)
+    print("It means X and Y is same showing that "" and '' doesnot matter at all")
+
+#Overwriting
+a = "John"
+a ="Hari"
+print("It prints the lastest input of the user called as overwriting",a)
+# But the CAPITAL letter does not overwrite
+
