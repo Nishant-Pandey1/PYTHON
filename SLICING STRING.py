2a0cc8ac3cf1ab7e13821545918f837210238b79
Nishant <pandeynishnu01@gmail.com>
2022-07-03 17:33:21 +0545

    Slicing the string means taking it's letters

---
+++ SLICING STRING.py
@@ -0,0 +1,16 @@
+# slicing a string
+# It just helps to print out the LETTERS on the given string
+# From next letter of the starting index to the end indexed letter
+b = "Nishant Pandey"
+print(b[1:5])
+# It prints isha
+
+# slicing from a index to end
+b = "Nishant Pandey"
+print(b[3:])
+# It prints isha
+
+b = "Hello, World!"
+print(b[-5:-2])
+#Negative sign count from the end
+
